2a3cda9e0a04277140eb6613bd05354be2f4dfd4
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-02-12 11:16:22 +0530

    update fileRoute

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -5,6 +5,7 @@ const { authenticateToken } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', authenticateToken, fileController.getFiles);
-
+router.get('/:fileId', authenticateToken, fileController.getFileById);
+router.delete('/:fileId', authenticateToken, fileController.deleteFile);
 
 module.exports = router;
